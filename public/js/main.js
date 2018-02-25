d135b88006fe4c2d7ffc0903bac935363e620db3
nqvst <jack.enqvist@gmail.com>
2018-02-25 12:27:09 +0100

    ep:3 jumping and update loop

--- public/js/main.js
+++ public/js/main.js
@@ -1,79 +1,49 @@
+import Compositor from './compositor.js';
+import Entity from './entity.js';
+import Timer from './timer.js';
 import { loadLevel } from './loaders.js';
-import { loadCharacterSprite, loadBackgroundSprites } from './sprites.js'
+import { loadBackgroundSprites } from './sprites.js'
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; x++) {
-            for (let y = y1; y < y2; y++) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
+import { createMario } from './entities.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-class Compositor {
-    constructor() {
-        this.layers = [];
-    }
-
-    draw(context) {
-        this.layers.forEach(layer => {
-            layer(context);
-        });
-    }
-}
-
-function createBackgroundLayer(backgrounds, sprites) {
-    const buffer = document.createElement('canvas');
-    buffer.width = 256;
-    buffer.height = 240;
-
-
-    backgrounds.forEach( bg => {
-        drawBackground(bg, buffer.getContext('2d'), sprites);
-    });
-
-    return function drawBackgroundLayer(context) {
-        context.drawImage(buffer, 0, 0);
-    }
-}
-
-function createSpriteLayer(sprite, pos) {
-    return function drawPriteLayer(context) {
-        sprite.draw('idle', context, pos.x, pos.y);
-    }
-}
-
 Promise.all([
-    loadCharacterSprite(),
+    createMario(),
     loadBackgroundSprites(),
     loadLevel('1-1'),
-]).then(([characterSprite, sprites, level])=> {
+]).then(([mario, bgSprite, level])=> {
 
     const comp = new Compositor();
 
-    const bgLayer = createBackgroundLayer(level.backgrounds, sprites);
+    const bgLayer = createBackgroundLayer(level.backgrounds, bgSprite);
 
-    comp.layers.push(bgLayer);
+    //comp.layers.push(bgLayer);
 
-    const pos = {
-        x: 64,
-        y: 64,
-    };
-    const spriteLayer = createSpriteLayer(characterSprite, pos);
+    const gravity = 30;
+    mario.pos.set(64, 180);
+    mario.vel.set(200, -600);
+
+    
+
+    const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
+    const deltaTime = 1/60;
+    let accTime = 0;
+    let lastTime = 0;
 
-    function update() {
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
         comp.draw(context);
-        characterSprite.draw('idle', context, pos.x, pos.y);
-        pos.x += 2;
-        requestAnimationFrame(update);
+        mario.update(deltaTime);
+        mario.vel.y += gravity;
+        accTime -= deltaTime;
+    
     }
 
-    update();
+    timer.start();
 });
 
